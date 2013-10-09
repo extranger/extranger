21aaeac904cb2c83368a2d5f1302f9c56afd5483
wzhjay <wzhjay@gmail.com>
2013-10-09 21:04:22 +0800

    connect with facebook api and get user data

--- application/controllers/FB.php
+++ application/controllers/FB.php
@@ -14,22 +14,46 @@ class FB extends CI_Controller
 
 	function index()
 	{
+
+	}
+
+	function login()
+	{
 		$facebook = new Facebook(array(
 		  'appId'  => '551637974908042',
-		  'secret' => '6f05a1f59eed5377c6cc560737255bb6',
+		  'secret' => '6f05a1f59eed5377c6cc560737255bb6'
+		  //'cookie' => true
 		));
+		//var_dump($facebook);
 		// Get User ID
 		$user = $facebook->getUser();
+		//var_dump($user);
+		$access_token = $facebook->getAccessToken();
+		//var_dump($access_token);
+		$friends = $facebook->api('/me/friends');
+		//var_dump($friends);
 
-		echo $user;
-		if ($user) {
-		  try {
-		    // Proceed knowing you have a logged in user who's authenticated.
-		    $user_profile = $facebook->api('/me');
-		  } catch (FacebookApiException $e) {
-		    error_log($e);
-		    $user = null;
-		  }
+		$pic = $facebook->api("https://graph.facebook.com/wzhjay/picture");
+		var_dump($pic);
+		echo "<img src='https://graph.facebook.com/100001294432614/picture?type=large'>";
+		if($user){
+			try {
+				$user_profile = $facebook->api('/me');
+				//var_dump($user_profile);
+			}
+			catch(FacebookApiException $e){
+				echo $e->getMessage();
+			}
+		}
+
+		if($user){
+			$logoutUrl = $facebook->getLogoutUrl();
+			//var_dump($logoutUrl);
+			echo "<a href='$logoutUrl'>LogoutUrl</a>";
+		}
+		else{
+			$loginUrl = $facebook->getLoginUrl();
+			echo "<a href='$loginUrl'>LoginUrl</a>";
 		}
 	}
 }
